5965852900b85e0ff961ae3e3da400f5cd4422e9
Dawwie <32976041+Dawwie@users.noreply.github.com>
2019-03-10 19:44:22 +0100

    Dodana funkcja beginCoins
    żeby wiedziało ile razy może użyć danej monety, zważając na to że już się jakiś nominał skończył (równa się 0)

--- VendingMaschineRec.py
+++ VendingMaschineRec.py
@@ -1,9 +1,9 @@
 Coins = {
-        500: 1,
+        500: 10,
         200: 10,
         100: 10,
         50: 10,
-        20: 10,
+        20: 0,
         10: 10,
         5: 1000,
         2: 1000,
@@ -14,7 +14,8 @@ Products = {
     "Sok":150,
     "Wafelek":80,
     "Rogal":200,
-    "Orzeszki":170
+    "Orzeszki":170,
+    "Baton":140
             }
 money = [500,200,100,50,20,10,5,2,1]
 def WriteProducts():
@@ -25,6 +26,16 @@ def EmptyCoins(): # Jeżeli nie ma jakiegoś nominału w automacie to dodaje do 
     for p in Coins.keys():
         if Coins[p] == 0:
             money.remove(p)
+
+def beginCoins(allCoins,firstCoin,n):
+    count = 0
+    for i in range(n):
+        allCoins -= firstCoin
+        if allCoins > 0:
+            count += 1
+        else:
+            pass
+    return count
 
 def change(n,available_coins,coins_so_far):
     if sum(coins_so_far) == n:
@@ -47,13 +58,9 @@ EmptyCoins()
 highestCoin = max(s for s in Coins.keys() if s <= rest if s in money) #znajduje pierwsza najwieksza liczbe mniejsza od reszty
 money.remove(highestCoin) #usuwam ją z listy
 numberOfCoins = Coins[highestCoin] #patrze ile razy moge jej użyć
+beginCoins(rest,highestCoin,numberOfCoins)
 
-if highestCoin > rest:
-    while highestCoin*numberOfCoins > rest: #zmienjszam jej mnożnik az bedzie mniejsza od reszty
-        numberOfCoins -= 1
-    solutions = [s for s in change(rest, money, [highestCoin]*numberOfCoins)]    #dodaje do poczatkowej listy jej wartość tyle razy ile może sie w niej znaleźć
-elif highestCoin < rest:
-    solutions = [s for s in change(rest,money,[highestCoin])]
-else:
-    solutions = [s for s in change(rest, money,[highestCoin])]
+
+
+solutions = [s for s in change(rest, money,[highestCoin]*beginCoins(rest,highestCoin,numberOfCoins))]
 print("Reszta: ", rest, " = ", min(solutions, key=len), "gr")
